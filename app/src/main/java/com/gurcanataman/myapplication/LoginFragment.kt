7bcdcd1ae06fb587abebb67958adb6d39e92bfb5
Gürcan ATAMAN <gurcanataman@gmail.com>
2021-02-01 00:09:35 +0300

    Initial commit

---
+++ app/src/main/java/com/gurcanataman/myapplication/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.gurcanataman.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            view?.let {
+                Navigation.findNavController(it).navigate(R.id.mainFragment, null)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/gurcanataman/myapplication/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.gurcanataman.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bottomNavigationView =
+            view.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        val navHostFragment =
+            (childFragmentManager.findFragmentById(R.id.nav_host_bottom) as NavHostFragment)
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
+
+    }
+}
